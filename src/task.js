ebc3bcb1e27d54f162c1ae8695aa75f4abafe2c6
github username <github email address>
2019-03-13 14:42:30 -0400

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,16 @@
-class Task {
-}
+// users should be able to type a task into the input field
+// * take input and insert it into ul section of div
+
+//users should be able to click some form of a submit button
+
+//the task string that the user provided should appear on the DOM after the submit button has been activated
+
+// const formTag = document.querySelector("form")
+//
+// formTag.addEventListener('submit', function(event){
+//     console.log(event);
+// })
+
+// class Task {
+//
+// }
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,56 @@
-class TaskList {
+// each list item is created with a delete button
+// each list created has a submit button that deletes li (parent element)
+const ulTag = document.getElementById("tasks")
+const formTag = document.querySelector("form")
+const inputId = document.getElementById("new-task-description")
+
+formTag.addEventListener('submit', addItemToList)
+
+ulTag.addEventListener('click', deleteItemFromList)
+
+function deleteItemFromList(event) {
+    event.preventDefault();
+    if (event.target.nodeName === "BUTTON"){
+        event.target.parentElement.remove()
+    }
+    // debugger
 }
+
+
+function addItemToList(e){
+    e.preventDefault();
+
+    let inputValue = inputId.value;
+    ulTag.innerHTML +=
+    `<li>
+      <div>${inputValue}</div>
+      <button>X</button>
+     </li>`
+}
+
+
+
+
+
+
+    // const liTags = document.querySelectorAll('li')
+
+    // for(let i = 0; i < liTags.length; i++){
+    //     liTags[i].lastElementChild.addEventListener('click', function(e){
+    //         alert('clicked');
+    //         e.preventDefault();
+    //         // const list = buttonTag.parentNode
+    //         // const ul = list.parentNode
+    //         // ul.removeChild(list)
+    //     })
+    // }
+
+
+
+
+// deletes parent of li button
+
+// function deleteNameFromList(e){
+//     e.preventDefualt();
+//
+// }
